05561a25deca3ee3a4398cda2670d30417b2680c
Luca <lucat1@users.noreply.github.com>
2019-10-13 12:56:05 +0200

    fix watch script and include a sample configuration

---
+++ config.sample.ts
@@ -0,0 +1,15 @@
+import DevLogger from "@jukebox/dev-logger";
+import FlatWorld from "@jukebox/flat-world";
+
+export default {
+  worlds: [
+    /* index 0 in the array will be the default world */
+    new FlatWorld({
+      name: "world",
+      surfaceBlock: "GRASS"
+    })
+  ],
+  logger: new DevLogger({ saveToFile: false }),
+  filesystem: null, // Can optionally be in memory
+  plugins: []
+};
